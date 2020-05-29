273bced6da97d5630e4b8c94442f6e206be64fbb
Zac Solomon <zac@zacsolo.com>
2020-05-29 11:20:30 -0400

    🌮 Remove Boiler, Add App and Index

---
+++ Streams/client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div>App</div>
+    </BrowserRouter>
+  );
+}
