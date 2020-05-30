7a1a35dbdcc3706420db9a7e6388729d6434d7e0
Zac Solomon <zac@zacsolo.com>
2020-05-30 19:57:29 -0400

    🌮Wire Inputs up in StreamCreate

--- Streams/client/src/components/streams/StreamCreate.js
+++ Streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default function StreamCreate() {
-  return <div>StreamCreate</div>;
+export class StreamCreate extends Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+  render() {
+    return (
+      <form>
+        <Field name='title' component={this.renderInput} />
+        <Field name='description' component={this.renderInput} />
+      </form>
+    );
+  }
 }
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
