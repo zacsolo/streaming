a3a4f0e41575ee2034b9cda268c60cefea0b5ebc
Zac Solomon <zac@zacsolo.com>
2020-06-01 10:07:20 -0400

    🌮 Create Custom History and Router

--- Streams/client/src/components/App.js
+++ Streams/client/src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 export default function App() {
   return (
     <div className='ui container'>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path='/' exact component={StreamList}></Route>
@@ -19,7 +20,7 @@ export default function App() {
           <Route path='/streams/delete' exact component={StreamDelete}></Route>
           <Route path='/streams/show' exact component={StreamShow}></Route>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
