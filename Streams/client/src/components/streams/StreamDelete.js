6d487169f6ca14c9eb48de7dc2f1a0922f5d3dc9
Zac Solomon <zac@zacsolo.com>
2020-06-02 10:43:39 -0400

    🌮 Abstracted Modal Props for Reusability

--- Streams/client/src/components/streams/StreamDelete.js
+++ Streams/client/src/components/streams/StreamDelete.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 export default function StreamDelete() {
+  const actions = (
+    <React.Fragment>
+      <button className='ui button negative'>Delete</button>
+      <button className='ui button'>Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title='Delete Stream'
+        content='Are you sure you want to delete this stream?'
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
   );
 }
