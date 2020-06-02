c4b167fff9d5205d323626f6805aa499383e6c97
Zac Solomon <zac@zacsolo.com>
2020-06-02 09:16:30 -0400

    🌮Integrate StreamEdit with StreamForm

--- Streams/client/src/components/streams/StreamEdit.js
+++ Streams/client/src/components/streams/StreamEdit.js
@@ -1,16 +1,30 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamFrom from './StreamForm';
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamFrom
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -18,4 +32,12 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
+
+const newProfile = {
+  name: 'Sam',
+  age: 18,
+  favoriteColor: 'green',
+};
